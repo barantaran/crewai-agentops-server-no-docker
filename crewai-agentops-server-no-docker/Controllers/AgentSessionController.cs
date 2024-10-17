45201d7cb895117bfa2cf32181a1ec1967ae6d59
Dmitri Yourchev <d.yurchev@yandex.ru>
2024-10-17 14:34:28 +0600

    fix: separate Agent actions into its own controller

--- crewai-agentops-server-no-docker/Controllers/AgentSessionController.cs
+++ crewai-agentops-server-no-docker/Controllers/AgentSessionController.cs
@@ -23,15 +23,6 @@ namespace AgentopsServer.Controllers
             return await _context.AgentSessions.ToListAsync();
         }
         
-        [HttpPost("create_agent", Name = "create_agent")]
-        public async Task<ActionResult<Agent>> CreateAgent(Agent agent)
-        {
-            _context.Agents.Add(agent);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction(nameof(Get), new { id = agent.AgentId }, agent);
-        }
-
         [HttpPost("create_session", Name = "create_session")]
         public async Task<ActionResult<AgentSession>> CreateSession(AgentSession session)
         {
---
+++ crewai-agentops-server-no-docker/Controllers/AgentsController.cs
@@ -0,0 +1,61 @@
+﻿using AgentopsServer.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AgentopsServer.Controllers
+{
+    [ApiController]
+    [Route("V1")]
+    public class AgentsController : ControllerBase
+    {
+        private readonly ILogger<AgentSessionController> _logger;
+        private readonly AgentServerDbContext _context;
+
+        public AgentsController(ILogger<AgentSessionController> logger, AgentServerDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        [HttpGet("agentsAll")]
+        public async Task<ActionResult<IEnumerable<Agent>>> GetAgents()
+        {
+            if (_context.Agents == null)
+            {
+                return NotFound();
+            }
+            return await _context.Agents.ToListAsync();
+        }
+
+        [HttpGet("agent/{id}")]
+        public async Task<ActionResult<Agent>> GetAgent(int id)
+        {
+            if (_context.Agents == null)
+            {
+                return NotFound();
+            }
+            var agent = await _context.Agents.FindAsync(id);
+
+            if (agent == null)
+            {
+                return NotFound();
+            }
+
+            return agent;
+        }
+
+        [HttpPost("create_agent")]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(Agent))]
+        public async Task<ActionResult<Agent>> PostAgent(Agent agent)
+        {
+            if (_context.Agents == null)
+            {
+                return Problem("Entity set 'AgentServerDbContext.Agents'  is null.");
+            }
+            _context.Agents.Add(agent);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAgent", new { id = agent.AgentId }, agent);
+        }
+    }
+}
