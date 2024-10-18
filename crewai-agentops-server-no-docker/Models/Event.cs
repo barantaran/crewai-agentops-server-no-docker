d42169af66cf82ece35a6a81adaaee2c36c4b9ac
Dmitri Yourchev <d.yurchev@yandex.ru>
2024-10-18 20:15:30 +0600

    chore: align Event entity fields with AgentOps JSON schema

--- crewai-agentops-server-no-docker/Models/Event.cs
+++ crewai-agentops-server-no-docker/Models/Event.cs
@@ -5,19 +5,19 @@
     {
         [JsonIgnore]
         public Guid EventId { get; set; }
-
-        [JsonProperty("id")]
-        public string? EventExtGuid { get; set; }
+        public string? EventType { get; set; }
+        public Params? Params { get; set; }
+        public DateTime InitTimestamp { get; set; }
+        public DateTime EndTimestamp { get; set; }
 
         [JsonProperty("agent_id")]
         public string? AgentExtGuid { get; set; }
-        public string? EventType { get; set; }
-        public DateTime InitTimestamp { get; set; }
-        public DateTime EndTimestamp { get; set; }
-        public string? Model { get; set; }
-        public ICollection<Prompt>? Prompts { get; set; }
-        public Params? Params { get; set; }
+
+        [JsonProperty("id")]
+        public string? EventExtGuid { get; set; }
+        public ICollection<Prompt>? Prompt { get; set; }
         public int PromptTokens { get; set; }
         public int CompletionTokens { get; set; }
+        public string? Model { get; set; }
     }
 }
--- crewai-agentops-server-no-docker/Models/EventDto.cs
+++ crewai-agentops-server-no-docker/Models/EventDto.cs
@@ -1,14 +1,18 @@
-﻿namespace AgentopsServer.Models
+﻿using Newtonsoft.Json;
+
+namespace AgentopsServer.Models
 {
     public class EventDto
     {
-        public string? AgentId { get; set; }
         public string? EventType { get; set; }
+        public Params? Params { get; set; }
         public DateTime InitTimestamp { get; set; }
         public DateTime EndTimestamp { get; set; }
+        public string? AgentExtGuid { get; set; }
+        public string? EventExtGuid { get; set; }
         public List<PromptDto>? Prompt { get; set; }
-        public Params? Params { get; set; }
         public int PromptTokens { get; set; }
         public int CompletionTokens { get; set; }
+        public string? Model { get; set; }
     }
 }
